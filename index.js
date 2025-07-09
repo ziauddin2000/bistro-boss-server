3010f1f63c0962b248893bd414f882736d14ce63
ziauddin2000 <ziabackbencherstudio2000@gmail.com>
2025-07-09 12:55:43 +0600

    module 67

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zui8vyn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,16 +27,46 @@ async function run() {
 
     let menuCollection = client.db("bistroBoss").collection("menus");
     let reviewCollection = client.db("bistroBoss").collection("reviews");
+    let cartCollection = client.db("bistroBoss").collection("carts");
 
+    // Menus APIs
     app.get("/menus", async (req, res) => {
       let cursor = menuCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
+    // Reviews APIs
     app.get("/reviews", async (req, res) => {
       let cursor = reviewCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Cart APIs
+
+    // Get user cart by email
+    app.get("/carts", async (req, res) => {
+      let email = req.query.email;
+      let query = { email };
+      let cursor = cartCollection.find(query);
+      let result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Add cart item for user
+    app.post("/carts", async (req, res) => {
+      let item = req.body;
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+
+    // Delete cart item by id
+    app.delete("/carts/:id", async (req, res) => {
+      let id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
 
